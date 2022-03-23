89384b83ec6d3261cdcacb8c5618c7d91fe8b740
Sten Vallimäe <69051391+StenVallimae@users.noreply.github.com>
2022-03-23 22:13:27 +0200

    styled comp test

--- workoutapp/pages/index.js
+++ workoutapp/pages/index.js
@@ -1,3 +1,16 @@
+import styled from "styled-components";
 export default function () {
-  return <div>Hello</div>;
+  return (
+    <Wrapper className="main-container">
+      <h1>Hello</h1>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
